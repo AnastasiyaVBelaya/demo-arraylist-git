f4864b9c897abc6370c34de48c269b0e59547574
Anastasiya Belaya <anastasia-belaya@mail.ru>
2024-12-23 20:56:06 +0300

    Initial commit for cities

---
+++ src/main/java/by/it_academy/jd2/Main.java
@@ -0,0 +1,15 @@
+package by.it_academy.jd2;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cities= new ArrayList<>();
+        cities.add("Moscow");
+        cities.add("Minsk");
+        cities.add("Kiev");
+        cities.add("New York");
+        cities.add("Gomel");
+        System.out.println(cities);
+    }
+}
