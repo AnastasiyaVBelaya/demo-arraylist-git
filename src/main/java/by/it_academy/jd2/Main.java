6116d05a196cfd8c8f9d52be3087d8b572bedf45
Anastasiya Belaya <anastasia-belaya@mail.ru>
2024-12-23 21:22:14 +0300

    add finding the max of length city.

--- src/main/java/by/it_academy/jd2/Main.java
+++ src/main/java/by/it_academy/jd2/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> cities= new ArrayList<>();
+        ArrayList<String> cities = new ArrayList<>();
         cities.add("Moscow");
         cities.add("Minsk");
         cities.add("Kiev");
@@ -12,5 +12,14 @@ public class Main {
         cities.add("Gomel");
         System.out.println(cities);
         cities.set(2, "Prague");
+        int maxLength = cities.get(0).length();
+        String cityMaxLength = cities.get(0);
+        for (String city : cities) {
+            if (city.length() > maxLength) {
+                cityMaxLength = city;
+                maxLength=city.length();
+            }
+        }
+        System.out.println(cityMaxLength);
     }
 }
